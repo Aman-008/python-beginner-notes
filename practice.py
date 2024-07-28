8395daf72626543cc144722a2b38f3a69fde5030
Aman-008 <amanp9568@gmail.com>
2024-07-28 08:01:42 +0530

    commited

---
+++ practice.py
@@ -0,0 +1,34 @@
+# # using print()
+
+# print("Hello World")
+
+# # using type()
+
+# a = 10
+# print(type(a))
+
+# b = "Apple"
+# print(type(b))
+
+# c = 10.5
+# print(type(c))
+
+
+
+
+# 1.First  swap method
+# a = 5
+# b = 7
+# a,b = b,a
+# print(a)
+# print(b)
+
+
+# 2. Second swap method
+# a = 9
+# b = 8
+# c = a
+# a = b
+# b = c
+# print(a)
+# print(b)
