b44f17be6a8f593340fd504ec03b609d1da18c27
Aman-008 <amanp9568@gmail.com>
2024-08-06 01:13:33 +0530

    commited

---
+++ Jupyter_notebook/practice.py
@@ -0,0 +1,12 @@
+# print(2+2/((2+2)+(2**2)))
+# print(-3 - (3*-1))
+# print(2 -1*(2))
+# print(-1 -1*(-1))
+
+def to_celsius(x):
+    
+    '''Convert Fahrenheit to Celsius'''
+    return (x-32) * 5/9
+
+
+to_celsius(75)
