6861f57df6b88fddcdc3a97b5c27438a752d07bb
Aman-008 <amanp9568@gmail.com>
2024-08-12 16:58:23 +0530

    commited

--- Jupyter_notebook/practice.py
+++ Jupyter_notebook/practice.py
@@ -3,10 +3,23 @@
 # print(2 -1*(2))
 # print(-1 -1*(-1))
 
-def to_celsius(x):
+#def to_celsius(x):
     
-    '''Convert Fahrenheit to Celsius'''
-    return (x-32) * 5/9
+#    '''Convert Fahrenheit to Celsius'''
+#    return (x-32) * 5/9
 
 
-to_celsius(75)
+#to_celsius(75)
+'''
+name = "Aman"
+result = len(name)
+print("Hello " + name + ". Your lucky number is :  " + str(result))
+'''
+
+def circle_area(radius):
+    pi = 3.14
+    area = pi * (radius ** 2)
+    print(area)
+
+circle_area(5)
+#Output is 78.5
