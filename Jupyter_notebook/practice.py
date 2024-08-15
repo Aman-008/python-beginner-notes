140c835092002a531fb188c47421d1169dc21e14
Aman-008 <amanp9568@gmail.com>
2024-08-15 13:22:47 +0530

    commited

--- Jupyter_notebook/practice.py
+++ Jupyter_notebook/practice.py
@@ -29,4 +29,24 @@ print("Hello " + name + ". Your lucky number is :  " + str(result))
 # print("Yellow" > "Cyan")
 
 
-print(2+2*(-1))
+#print(2+2*(-1))
+
+
+# This function rounds a variable number up to the nearest 10x value
+def round_up(number):
+  x = 10
+# The floor division operator will calculate the integer value of
+# "number" divided by x: 35 // 10 will return the integer 3.
+  whole_number = number // x
+# The modulo operator will calculate the remainder value of "number"
+# divided by x: 35 % 10 will return the remainder value 5.
+  remainder = number % x
+# If the remainder is greater than or equal to 5: 
+  if remainder >= 5: 
+# Return x multiplied by the (whole_number+1) to round up
+    return x*(whole_number+1)
+# Else, return x multiplied by the whole_number to round down
+  return x*whole_number
+ 
+# Calls the function with the parameter value of 35.
+print(round_up(35)) # Should print 40
