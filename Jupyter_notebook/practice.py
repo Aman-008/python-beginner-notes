c9f7bc6158f3613afc41128799f34d87b486b51d
Aman-008 <amanp9568@gmail.com>
2024-08-16 01:38:32 +0530

    commited

--- Jupyter_notebook/practice.py
+++ Jupyter_notebook/practice.py
@@ -1,3 +1,37 @@
+# # using print()
+
+# print("Hello World")
+
+# # using type()
+
+# a = 10
+# print(type(a))
+
+# b = "Apple"
+# print(type(b))
+
+# c = 10.5
+# print(type(c))
+
+
+
+
+# 1.First  swap method
+# a = 5
+# b = 7
+# a,b = b,a
+# print(a)
+# print(b)
+
+
+# 2. Second swap method
+# a = 9
+# b = 8
+# c = a
+# a = b
+# b = c
+# print(a)
+# print(b)
 # print(2+2/((2+2)+(2**2)))
 # print(-3 - (3*-1))
 # print(2 -1*(2))
@@ -67,6 +101,7 @@ print("x=" + str(x))
 
 '''
 
+'''
 
 x = 1
 sum = 0
@@ -81,3 +116,48 @@ while x < 10:
 
 print(sum, product)
 #Ouput 45 1
+
+'''
+
+
+# This function counts the number of integer factors for a 
+# "given_number" variable, passed through the function’s parameters.
+# The "count" return value includes the "given_number" itself as a 
+# factor (n*1). 
+def count_factors(given_number):
+
+    # To include the "given_number" variable as a "factor", initialize
+    # the "factor" variable with the value 1 (if the "factor" variable
+    # were to start at 2, the "given_number" itself would be excluded). 
+    factor = 1
+    count = 1
+
+    # This "if" block will run if the "given_number" equals 0.
+    if given_number == 0:
+        # If True, the return value will be 0 factors. 
+        return 0
+
+    # The while loop will run while the "factor" is still less than
+    # the "given_number" variable.
+    while factor < given_number:
+        # This "if" block checks if the "given_number" can be divided by
+        # the "factor" variable without leaving a remainder. The modulo
+        # operator % is used to test for a remainder.
+        if given_number % factor == 0:
+            # If True, then the "factor" variable is added to the count of
+            # the "given_number"’s integer factors.
+            count += 1
+        # When exiting the if block, increment the "factor" variable by 1
+        # to divide the "given_number" variable by a new "factor" value
+        # inside the while loop.
+        factor += 1
+
+    # When the interpreter exits either the while loop or the top if
+    # block, it will return the value of the "count" variable.
+    return count
+
+
+print(count_factors(0)) # Count value should be 0
+print(count_factors(3)) # Should count 2 factors (1x3)
+print(count_factors(10)) # Should count 4 factors (1x10, 2x5)
+print(count_factors(24)) # Should count 8 factors (1x24, 2x12, 3x8, and 4x6). 
