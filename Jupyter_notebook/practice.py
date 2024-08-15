e03b26eae4e35c84bb3ed1b392740fd23b41bd58
Aman-008 <amanp9568@gmail.com>
2024-08-15 19:48:55 +0530

    commited

--- Jupyter_notebook/practice.py
+++ Jupyter_notebook/practice.py
@@ -66,3 +66,18 @@ while x < 5:
 print("x=" + str(x))
 
 '''
+
+
+x = 1
+sum = 0
+while x < 10:
+    sum = sum + x
+    x = x + 1
+
+product = 1
+while x < 10:
+    product = product * x
+    x = x + 1
+
+print(sum, product)
+#Ouput 45 1
