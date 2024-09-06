a2022c4ea2a5551d22860f259000a201729ada90
Aman-008 <amanp9568@gmail.com>
2024-09-07 01:30:58 +0530

    commited

--- Jupyter_notebook/Practice/fibonacci.py
+++ Jupyter_notebook/Practice/fibonacci.py
@@ -1,16 +1,28 @@
-# Update the '_' to solve the problem
+# # Update the '_' to solve the problem
+
+# n = int(input())
+# a = 0
+# b = 1
+
+# print(a, b, end=" ")
+
+# count = 1
+
+# for i in range(n-2):
+#     c = a+b # set currrent number as sum of previous two numbers
+#     print(c, end=" ")
+#     # Update a and b as next two numbers
+#     a = b 
+#     b = c
+
+
+
 
 n = int(input())
-a = 0
-b = 1
-
-print(a, b, end=" ")
-
-count = 1
-
-for i in range(n-2):
-    c = a+b # set currrent number as sum of previous two numbers
-    print(c, end=" ")
-    # Update a and b as next two numbers
-    a = b 
-    b = c
+# Solution as follows
+first, second = 0, 1
+for i in range(n):
+    print(first, end=" ")
+    next = first + second
+    first = second
+    second = next
